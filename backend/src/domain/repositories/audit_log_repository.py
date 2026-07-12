1e8504e929a611fe15ac97ea61971b948b155f78
Dhruvi <dhruvi2535@gmail.com>
2026-07-12 13:51:42 +0530

    Create audit_log_repository.py

---
+++ backend/src/domain/repositories/audit_log_repository.py
@@ -0,0 +1,5 @@
+from src.domain.entities.audit_log_entry import AuditLogEntry
+from src.domain.repositories.base_repository import BaseRepository
+
+class AuditLogRepository(BaseRepository[AuditLogEntry]):
+    pass
